a27152f9277e6b8093f8eb3311c6518997aac38b
Ian-Kelley <54553111+Ian-Kelley@users.noreply.github.com>
2020-02-05 13:55:36 -0500

    Update tecgif.py

--- tecgif.py
+++ tecgif.py
@@ -7,13 +7,14 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-import imageio
+import gc
+from PIL import Image, ImageDraw
+
 #Specify date here, 10/20/17 is already in the github repo, else download the date with hdf5_to_npz.py
 year = '2017'
 month = '10'
 day = '20'
-time = 0#madrigal's time index, starts at 0 being 0:02UT and increments by 5 minutes
-#range is 0-288 with 288 being 23:57 UT
+
 
 
 def timeconvert(time):
@@ -32,38 +33,39 @@ def plotter(Zm, time, lat, lon):
     plt.figure(figsize=(20, 15))
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.set_global()
+    
     #uncomment the following line for coastline projection 
     ax.coastlines()
     
     mesh = ax.pcolormesh(lon, lat, Zm, cmap='jet', vmax=30, transform=ccrs.PlateCarree()) 
     clrbar = plt.colorbar(mesh, shrink=0.5)
-    #cbar.set_label('Total Electron Content (TECU)')
+    clrbar.set_label('Total Electron Content (TECU)')
     plt.title("TEC for " + month + "/" + day + "/" + year + " at " + uthour + ':' + utminute +' UT')
-    plt.savefig(str(time) + '.png', format="png")
+    plt.savefig(str(time) + '.png', format="png", bbox_inches = 'tight')#saves figure as png
+    
+    #uncomment to show plot in IDE
+    #plt.show()
+    
+    gc.collect()
     plt.close()
-
-
-
-lon = np.linspace(-180, 180, 360)
-lat = np.linspace(-90, 90, 180)
-
-
 
 
 with np.load(year + month + day + '.npz') as data:
     tec = data['tec']
-    #dtec = data['dtec']
+    lon = np.linspace(-180, 180, 360)
+    lat = np.linspace(-90, 90, 180)
     images = []
+    time = 0
     while time < 288:
         Z = tec[:,:,time]        
         Zm = ma.masked_where(np.isnan(Z),Z)#masks NaNs to display as white on plot
-        del Z
+        del Z#Hoping to save a little memory
         plotter(Zm, time, lat, lon)
-
-        images.append(imageio.imread(str(time) + '.png'))
+        del Zm
+        images.append(Image.open(str(time) + '.png'))
         
         os.remove(str(time)+'.png')
         time = time + 1
         
-    imageio.mimsave('movie1.gif', images)
+    images[0].save('tec1.gif',save_all=True, append_images=images[1:], optimize=True)
     del images
